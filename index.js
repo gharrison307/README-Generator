5e73701864c07a239dbc3188690eb139eb737fcf
George Harrison <g.a.harrison91@gmail.com>
2023-03-05 13:01:50 -0700

    added license badges and hyperlink to info

--- index.js
+++ index.js
@@ -50,8 +50,8 @@ inquirer
         "BSD 2-Clause Simplified License",
         "BSD 3-Clause New or Revised License",
         "Boost Sofetware License 1.0",
-        "creative Commons Zero v1.0 Universal",
-        " Eclipse Public License 2.0",
+        "Creative Commons Zero v1.0 Universal",
+        "Eclipse Public License 2.0",
         "GNU Affero General Public License v3.0",
         "GNU Gerneral Public License v2.0",
         "GNU Lesser General Public liencese v2.1",
@@ -173,3 +173,47 @@ function createFile(data) {
 }
 
 // License Information Function
+
+const licenseBadge = (license) => {
+  switch (license) {
+    case "Apache 2.0":
+      return `[![License: Apache 2.0](https://img.shields.io/badge/license-Apache%202.0-blue)](https://www.apache.org/licenses/LICENSE-2.0)`;
+      break;
+    case "GNU General Public License v3.0":
+      return `[![License: GNU General Public License v3.0](https://img.shields.io/badge/license-GNU%20General%20Public%20License%20v3.0-blue)](https://www.gnu.org/licenses/gpl-3.0.en.html)`;
+      break;
+    case "MIT License":
+      return `[![License: MIT License](https://img.shields.io/badge/license-MIT-blue)](https://www.mit.edu/~amini/LICENSE.md)`;
+      break;
+    case "BSD 2-Clause Simplified License":
+      return `[![License: BSD 2-Clause Simplified License](https://img.shields.io/badge/license-BSD%202--Clause%20Simplified%20License-blue)](https://opensource.org/license/bsd-2-clause/)`;
+      break;
+    case "BSD 3-Clause New or Revised License":
+      return `[![License: BSD 3-Clause New or Revised License](https://img.shields.io/badge/license-BSD%203--Clause%20New%20or%20Revised%20License-blue)](https://choosealicense.com/licenses/bsd-3-clause/)`;
+      break;
+    case "Boost Sofetware License 1.0":
+      return `[![License: Boost Sofetware License 1.0](https://img.shields.io/badge/license-Boost%20Sofetware%20License%201.0-blue)](https://www.boost.org/LICENSE_1_0.txt)`;
+      break;
+    case "Creative Commons Zero v1.0 Universal":
+      return `[![License: Creative Commons Zero v1.0 Universal](https://img.shields.io/badge/license-Creative%20Commons%20Zero%20v1.0%20Universal-blue)](https://creativecommons.org/publicdomain/zero/1.0/)`;
+      break;
+    case "Eclipse Public License 2.0":
+      return `[![License: Eclipse Public License 2.0](https://img.shields.io/badge/license-Eclipse%20Public%20License%202.0-blue)](https://www.eclipse.org/legal/epl-2.0/)`;
+      break;
+    case "GNU Affero General Public License v3.0":
+      return `[![License: GNU Affero General Public License v3.0](https://img.shields.io/badge/license-GNU%20Affero%20General%20Public%20License%20v3.0-blue)](https://www.gnu.org/licenses/agpl-3.0.en.html)`;
+      break;
+    case "GNU Gerneral Public License v2.0":
+      return `[![License: GNU Gerneral Public License v2.0](https://img.shields.io/badge/license-GNU%20Gerneral%20Public%20License%20v2.0-blue)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)`;
+      break;
+    case "GNU Lesser General Public liencese v2.1":
+      return `[![License: GNU Lesser General Public liencese v2.1](https://img.shields.io/badge/license-GNU%20Lesser%20General%20Public%20liencese%20v2.1-blue)](https://www.gnu.org/licenses/old-licenses/lgpl-2.1.en.html)`;
+      break;
+    case "Mozilla Public License 2.0":
+      return `[![License: Mozilla Public License 2.0](https://img.shields.io/badge/license-Mozilla%20Public%20License%202.0-blue)](https://www.mozilla.org/en-US/MPL/2.0/)`;
+      break;
+    case "The Unilicense":
+      return `[![License: The Unilicense](https://img.shields.io/badge/license-The%20Unilicenselic%20License%202.0-blue)](https://unlicense.org/)`;
+      break;
+  }
+};
